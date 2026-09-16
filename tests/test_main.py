00f231cb1b0e67f8ce318cd3a7f8eb779d2ee59f
matthew633jdi <matthew633jdi@gmail.com>
2026-09-16 15:42:00 +0900

    add fastapi and pytest, github actions

---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root_status_code():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+
+def test_read_root_body():
+    response = client.get("/")
+    assert response.json() == {'Hello': 'World'}
